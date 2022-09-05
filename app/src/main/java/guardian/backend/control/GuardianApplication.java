25f9ce93159576d78be2ca719d845f22727c7bce
valmojr <val_mo_jr@hotmail.com>
2022-09-04 23:04:32 -0300

    Updated SpringBoot Sv

--- app/src/main/java/guardian/backend/control/GuardianApplication.java
+++ app/src/main/java/guardian/backend/control/GuardianApplication.java
@@ -9,5 +9,4 @@ public class GuardianApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GuardianApplication.class, args);
 	}
-
 }
--- app/src/main/java/guardian/backend/control/Registrator.java
+++ app/src/main/java/guardian/backend/control/Registrator.java
@@ -1,7 +1,6 @@
 package guardian.backend.control;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 
 import guardian.backend.model.AreaOfInterest;
