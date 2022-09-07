7916872a3858685d312fd7b9b270e1f5f7b06ce0
Valmo <val_mo_jr@hotmail.com>
2022-09-07 18:24:42 -0300

    REST API w/ Spring Boot Progress

--- app/src/main/java/guardian/backend/control/GuardianApplication.java
+++ app/src/main/java/guardian/backend/control/GuardianApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GuardianApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GuardianApplication.class, args);
+		System.out.println("Server started...");
 	}
-
 }
--- app/src/main/java/guardian/backend/control/Registrator.java
+++ app/src/main/java/guardian/backend/control/Registrator.java
@@ -15,6 +15,10 @@ public class Registrator {
         firstResponder.setFirstResponderType(firstResponderType);
         new FirstResponderDAO().create(firstResponder);
     }
+    public void registerTrooper(FirstResponder firstResponder) {
+        firstResponder.setPrivilegeLevel(1);
+        new FirstResponderDAO().create(firstResponder);
+    }
     public void registerCommsOperator(String username, String password, String email, String showedName, String description, int firstResponderType) {
         FirstResponder firstResponder = new FirstResponder();
         firstResponder.setUsername(username);
@@ -24,6 +28,25 @@ public class Registrator {
         firstResponder.setDescription(description);
         firstResponder.setPrivilegeLevel(2);
         firstResponder.setFirstResponderType(firstResponderType);
+        new FirstResponderDAO().create(firstResponder);
+    }
+    public void registerCommsOperator(FirstResponder firstResponder) {
+        firstResponder.setPrivilegeLevel(2);
+        new FirstResponderDAO().create(firstResponder);
+    }
+    public void registerAdmin(String username, String password, String email, String showedName, String description, int firstResponderType) {
+        FirstResponder firstResponder = new FirstResponder();
+        firstResponder.setUsername(username);
+        firstResponder.setPassword(new Authenticator().encryptate(password));
+        firstResponder.setEmail(email);
+        firstResponder.setShowedName(showedName);
+        firstResponder.setDescription(description);
+        firstResponder.setPrivilegeLevel(3);
+        firstResponder.setFirstResponderType(firstResponderType);
+        new FirstResponderDAO().create(firstResponder);    
+    }
+    public void registerAdmin(FirstResponder firstResponder) {
+        firstResponder.setPrivilegeLevel(3);
         new FirstResponderDAO().create(firstResponder);
     }
 }
